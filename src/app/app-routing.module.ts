457b0beb8983c0f6987dc54181717b6d86448224
Samuel Lúcio <samuel.lucio@quark.tec.br>
2024-11-13 12:06:03 -0300

    ng2-charts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NgxChartsComponent } from './ngx-charts/ngx-charts/ngx-charts.component';
+import { Ng2ChartsComponent } from './ng2-charts/ng2-charts/ng2-charts.component';
 
 const routes: Routes = [
-  {path: "",component: NgxChartsComponent}
+  {path: "",component: NgxChartsComponent},
+  {path: "/ng2-charts",component: Ng2ChartsComponent}
 ];
 
 @NgModule({
--- src/app/ng2-charts/ng2-charts.module.ts
+++ src/app/ng2-charts/ng2-charts.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Ng2ChartsRoutingModule } from './ng2-charts-routing.module';
 import { Ng2ChartsComponent } from './ng2-charts/ng2-charts.component';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -12,10 +14,12 @@ import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
   ],
   imports: [
     CommonModule,
-    Ng2ChartsRoutingModule
+    Ng2ChartsRoutingModule,
+    BrowserModule
   ],
   providers: [
     provideCharts(withDefaultRegisterables())
-  ]
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Ng2ChartsModule { }
