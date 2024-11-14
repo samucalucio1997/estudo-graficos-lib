2197acc475ed61608168685ee0f127859454880f
Samuel Lúcio <samuel.lucio@quark.tec.br>
2024-11-14 11:58:04 -0300

    cores funcionando

--- src/app/ng2-charts/ng2-charts/ng2-charts.component.ts
+++ src/app/ng2-charts/ng2-charts/ng2-charts.component.ts
@@ -29,8 +29,8 @@ export class Ng2ChartsComponent {
   public barChartData: ChartData<'bar'> = {
     labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+      // { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A',backgroundColor: 'rgba(255, 99, 132, 0.2)', borderColor: 'rgba(255, 99, 132, 1)', borderWidth: 1 },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B',  backgroundColor: 'rgba(54, 162, 235, 0.2)', borderColor: 'rgba(54, 162, 235, 1)', borderWidth: 1},
     ],
   };
 
